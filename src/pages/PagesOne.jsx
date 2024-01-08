02bf5757f52db024d05ff8fdd8d3d3de8d0d8a60
Noreen-Khoso <noreenkhoso0@gmail.com>
2024-01-08 18:21:40 +0500

    --

--- src/pages/PagesOne.jsx
+++ src/pages/PagesOne.jsx
@@ -7,8 +7,8 @@ const PagesOne = () => {
         
             <div className=' flex items-center flex-col gap-4 '>
             <h1 className=' text-2xl font-bold text-blue-400'>Page one</h1>
-                <input type="text" placeholder='Type' className='px-10 py-2 focus:outline-none bg-white text-black/20' />
-                <textarea name="" id="" className="Px-10 py-20"></textarea>
+                <input type="text" placeholder='Type' className='px-10 py-2 focus:outline-none bg-white text-black/50' />
+                <textarea name="" id=""   className=' mx-10 px-10 text-lg py-2 text-black/50 focus:outline-none'></textarea>
 
             </div>
 
